22f0ba24561c3b64abbf2936d896d14d2bf20f7a
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2015-01-21 23:08:21 -0500

    format readme->godoc

--- doc.go
+++ doc.go
@@ -1,19 +1,19 @@
 /*
-GoWorker
-====
-A plugin based work management system implimented in pure go.
+Package GoWorker
 
+A plugin based work management system implimented in pure go.
 This project is still under heavy development. APIs are subject to change.
 
-## Installation
+Installation
+
 To install GoWorker, simply run
 `go get github.com/barracudanetworks/GoWorker/...`
 
-##### Hint
 In order for this to work, you must have a valid GOPATH set
 (read more about GOPATH [here](https://code.google.com/p/go-wiki/wiki/GOPATH))
 
-## Testing
+Testing
+
 To run all tests for the project simply navigate to the root of the project folder and run
 `go test ./...`
 
@@ -25,7 +25,8 @@ To run benchmarks contained in a package's tests run
 
 More on testing in go can be found [here](http://www.golang-book.com/12/index.htm)
 
-## Usage
+Usage
+
 The main program for this project can be built by navigating to cmd/goworker/ and either running
 `go run goworker.go` for testing purproses.
 
@@ -33,7 +34,8 @@ or
 
 `go build` to compile the full binary for repeated use.
 
-## Composition
+Composition
+
 The work manager is compried of three main componants.
 * Provider
 	* Provides jobs to the manager from an external source.
@@ -42,7 +44,8 @@ The work manager is compried of three main componants.
 * Manager
 	* Manages the work pipline. Requests jobs from the providers and sends them to the workers.
 
-### Pipeline
+Pipeline
+
 There are three main stages in the worker pipeline.
 
 1. The manager requests a job from the provider.
@@ -53,10 +56,11 @@ So graphically...
 __Provider->Manager->Worker->Manager__
 where each arrow represents a transition of ownership of the job.
 
-## Plugins
+Plugins
+
 Workers and providers are implemented as plugins. Because go compiles down to a static binary, you must recompile when you add new plugins.
 To load your plugin into the binary, add a blank import to your package in the main go program inside of the cmd directory. Your package must have an init function that calls either `provider.LoadProvider(YourProviderFactory)` or `worker.LoadWorker(YourWorkerFactory)`. Once your worker or provider has been loaded, it will be accessable in the config file by its name, in all lowercase.
 
-More on writing plugins [worker](http://godoc.org/github.com/barracudanetworks/GoWorker/worker) [provider](http://godoc.org/github.com/barracudanetworks/GoWorker/provider)
+More on writing plugins http://godoc.org/github.com/barracudanetworks/GoWorker/worker http://godoc.org/github.com/barracudanetworks/GoWorker/provider
 */
 package GoWorker
